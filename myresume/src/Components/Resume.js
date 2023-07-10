fa061a4f68661a823f498d89f23333dbffa3df9e
Pratik Patel <pratspatel17791@gmail.com>
2023-07-10 00:20:06 -0500

    Pratik Patel - PE01

---
+++ myresume/src/Components/Resume.js
@@ -0,0 +1,83 @@
+import { React } from 'react';
+
+function Resume() {
+  return (
+    <div className="Resume">
+      <div style={{ backgroundColor: "#bdfb04" }}>
+        {" "}
+        &nbsp;
+        <h1>
+          <b>Student Name </b>
+        </h1>
+        <h4>3900 Main street, Mount Prospect, IL, 60056 | (630)-303-8596 |</h4>
+        <h4> pratikpatel@cityuniversity.edu</h4>
+        &nbsp;
+        <h1>
+          <b>Education </b>
+        </h1>
+        <h2> Master of Science in Computer Science </h2>
+        <h4> City University of Seattle, Seattle, WA, USA | December 2023</h4>
+        <h4> GPA : 3.7 / 4.0</h4>
+        <h2>
+          Bachelors of Science in Computer Science with Software Development{" "}
+        </h2>
+        <h4> Northern Illinoise University, DeKalb, IL, USA | August 2020 </h4>
+        <h4> GPA : 3.3 / 4.0</h4>&nbsp;
+        <h1>Skills</h1>
+        <h2>Programing Language: Java, Python, Javascript</h2>
+        <h2>
+          Framework and Libraries: ReactJs, Angular SpringBoot, NodeJs,
+          ExpressJS
+        </h2>
+        <h4>Operating system: Mac, Windows, Linux</h4>
+        <h4>Database System : MySQL, MongoDB</h4>
+        <h4>Version Control : Git, GitHub</h4>&nbsp;
+        <h1>Work Experience</h1>
+        <h2>Software Engineer</h2>
+        <h3>American Express, Phoenix, AZ, USA, Septmber 2021 - Current </h3>
+        <h4>
+          Working as a Full Stack Developer using ReactJS in front-end and
+          Spring Boot in back-end.
+        </h4>
+        <h4>Unit testing using VS code.</h4>
+        <h4>Provide support to create APIs and test using Postman</h4>
+        <h2>Java Developer at Sears Holding Corporation</h2>
+        <h4>
+          Working as a Full Stack Developer using ReactJS in front-end and
+          Spring Boot in back-end.
+        </h4>
+        <h4>Unit testing using VS code.</h4>
+        <h4>Provide support to create APIs and test using Postman</h4>&nbsp;
+        <h1>Projects</h1>
+        <h2>Online Book store</h2>
+        <h4>
+          {" "}
+          Created personal Websites and deployed in Github using Git version
+          Control
+        </h4>
+        <h4>
+          source Code:{" "}
+          <a href="https://www.pluralsight.com/guides/change-page-background-color-each-routegoogle.com">
+            https://www.pluralsight.com/guides/change-page-background-color-each-routegoogle.com
+          </a>
+        </h4>
+        <h2>PubHub App</h2>
+        <h4>
+          Created full stack Application using Java, java servlets, PgAdmin4 and
+          Eclipes
+        </h4>
+        <h4>
+          {" "}
+          Source Code:{" "}
+          <a href="https://www.pluralsight.com/guides/change-page-background-color-each-routegoogle.com">
+            https://www.pluralsight.com/guides/change-page-background-color-each-routegoogle.com
+          </a>
+        </h4>
+        <div>&nbsp;</div>
+      </div>
+    </div>
+  );
+  
+}
+
+export default Resume;
